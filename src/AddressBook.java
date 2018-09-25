f65b5a1b48f8139c628c937cd8fc9c3faa01d539
thomas <thomaselmoleung@gmail.com>
2018-09-24 23:29:30 -0400

    edited code

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -2,26 +2,33 @@ import java.util.ArrayList;
 
 public class AddressBook {
 
- private ArrayList<BuddyInfo> buddyInfo;
-	
-	public AddressBook(String name) {
-		
+	private ArrayList<BuddyInfo> buddyInfo;
+
+	public AddressBook() {
+
 		buddyInfo = new ArrayList<BuddyInfo>();
-		
+
 	}
-	
+
 	public void addBuddy(BuddyInfo info) {
-		
-		buddyInfo.add(info);
+		if (info != null) {
+			buddyInfo.add(info);
+		}
 	}
-	
-	public void removeBuddy(int index) {
-		
-		buddyInfo.remove(index);
+
+	public BuddyInfo removeBuddy(int index) {
+		if (index >= 0 && index < this.buddyInfo.size()) {
+			return this.buddyInfo.remove(index);
+		}
+		return null;
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("Address Book");
+		BuddyInfo buddy = new BuddyInfo("Chris", "Sesame Street", 23456789);
+		AddressBook addressBook = new AddressBook();
+		addressBook.addBuddy(buddy);
+		addressBook.removeBuddy(0);
 	}
 }
